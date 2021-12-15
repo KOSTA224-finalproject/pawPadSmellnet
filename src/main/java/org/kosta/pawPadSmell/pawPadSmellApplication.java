3d10f927ef6dcd59f7f3075b24a273389e9cd095
ssjjkwak <ssjjkwak@naver.com>
2021-12-15 17:34:19 +0900

    first commit

---
+++ src/main/java/org/kosta/pawPadSmell/pawPadSmellApplication.java
@@ -0,0 +1,13 @@
+package org.kosta.pawPadSmell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class pawPadSmellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(pawPadSmellApplication.class, args);
+	}
+
+}
